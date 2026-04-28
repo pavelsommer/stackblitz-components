81a776e9071f955a58b1d0c509596f075dcc2ad1
paYa <pavel.sommer@gmail.com>
2026-04-29 01:09:46 +0200

    sync!

--- src/test5/index.js
+++ src/test5/index.js
@@ -1,3 +1,19 @@
+import createState from "./core/state.js";
 import createButton from "./components/Button";
 
-customElements.define("my-button", createButton());
+const stateTarget = new EventTarget();
+const state = createState(stateTarget);
+
+stateTarget.addEventListener("prop-changed", (event) => {
+	console.log("State changed:", event.detail);
+});
+
+customElements.define(
+	"my-button",
+	createButton(() => state),
+);
+
+customElements.define(
+	"x-button",
+	createButton(() => state),
+);
