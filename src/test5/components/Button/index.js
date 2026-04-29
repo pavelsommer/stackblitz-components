6c6c451acfc93e6002d0c5763c180cc80bacfcb1
paYa <pavel.sommer@gmail.com>
2026-04-29 03:15:36 +0200

    sync!

--- src/test5/components/Button/index.js
+++ src/test5/components/Button/index.js
@@ -4,114 +4,125 @@ import createState from "./../../core/state.js";
 const template = document.createElement("template");
 
 template.setHTMLUnsafe(
-	`<h1></h1><hr /><button></button><button></button>
+  `<h1></h1><hr /><button></button><button></button>
 <hr />
 <input type="text" />`,
 );
 
 export default (getState) => {
-	return class extends createComponent(template) {
-		#props = null;
-		#eventTarget = null;
+  return class extends createComponent(template) {
+    #props = null;
+    #eventTarget = null;
 
-		constructor() {
-			super();
+    constructor() {
+      super();
 
-			const { proxy, eventTarget } =
-				(getState && getState()) ?? createState(this);
+      const { proxy, eventTarget } =
+        (getState && getState()) ?? createState(this);
 
-			this.#props = proxy;
-			this.#eventTarget = eventTarget;
-		}
+      this.#props = proxy;
+      this.#eventTarget = eventTarget;
+    }
 
-		connectedCompleteCallback() {
-			const countName = this.dataset.countName ?? "count";
+    get h1Element() {
+      return this.shadowRoot.children[0];
+    }
 
-			if (typeof this.#props[countName] !== "number") {
-				this.#props[countName] = 0;
-			}
+    get incrementButton() {
+      return this.shadowRoot.children[2];
+    }
 
-			(() => {
-				const element = this.shadowRoot.children[0];
+    get decrementButton() {
+      return this.shadowRoot.children[3];
+    }
 
-				if (this.#props && this.#eventTarget) {
-					element.textContent = `The current count is: ${this.#props[countName]}`;
+    get inputElement() {
+      return this.shadowRoot.children[5];
+    }
 
-					this.#eventTarget.addEventListener("prop-changed", (event) => {
-						if (event.detail.key === countName) {
-							element.textContent = `The current count is: ${event.detail.value}`;
-						}
-					});
-				}
-			})();
+    connectedCompleteCallback() {
+      const countName = this.dataset.countName ?? "count";
+      const hasProps = this.#props && this.#eventTarget;
 
-			(() => {
-				const element = this.shadowRoot.children[2];
+      if (typeof this.#props[countName] !== "number") {
+        this.#props[countName] = 0;
+      }
 
-				if (this.#props && this.#eventTarget) {
-					element.textContent = `increment: ${this.#props[countName]}`;
+      hasProps &&
+        ((element) => {
+          element.textContent = `The current count is: ${this.#props[countName]}`;
+        })(this.h1Element);
 
-					this.#eventTarget.addEventListener("prop-changed", (event) => {
-						if (event.detail.key === countName) {
-							element.textContent = `increment: ${event.detail.value}`;
-						}
-					});
+      hasProps &&
+        ((element) => {
+          element.textContent = `increment: ${this.#props[countName]}`;
 
-					element.addEventListener(
-						"click",
-						(event) => this.#props[countName]++,
-					);
-				}
-			})();
+          element.addEventListener(
+            "click",
+            (event) => this.#props[countName]++,
+          );
+        })(this.incrementButton);
 
-			(() => {
-				const element = this.shadowRoot.children[3];
+      hasProps &&
+        ((element) => {
+          element.textContent = `decrement: ${this.#props[countName]}`;
 
-				if (this.#props && this.#eventTarget) {
-					element.textContent = `decrement: ${this.#props[countName]}`;
+          element.addEventListener(
+            "click",
+            (event) => this.#props[countName]--,
+          );
+        })(this.decrementButton);
 
-					this.#eventTarget.addEventListener("prop-changed", (event) => {
-						if (event.detail.key === countName) {
-							element.textContent = `decrement: ${event.detail.value}`;
-						}
-					});
+      hasProps &&
+        ((element) => {
+          element.value = `The current count is: ${this.#props[countName]}`;
 
-					element.addEventListener(
-						"click",
-						(event) => this.#props[countName]--,
-					);
-				}
-			})();
+          element.addEventListener("input", (event) => {
+            const value = parseInt(event.target.value, 10);
 
-			(() => {
-				const element = this.shadowRoot.children[5];
+            if (!isNaN(value)) {
+              this.#props[countName] = value;
+            }
+          });
 
-				if (this.#props && this.#eventTarget) {
-					element.value = `The current count is: ${this.#props[countName]}`;
+          element.addEventListener("focus", (event) => {
+            event.target.value = `${this.#props[countName]}`;
+            event.target.select();
+          });
 
-					this.#eventTarget.addEventListener("prop-changed", (event) => {
-						if (event.detail.key === countName && !element.matches(":focus")) {
-							element.value = `The current count is: ${event.detail.value}`;
-						}
-					});
+          element.addEventListener("blur", (event) => {
+            event.target.value = `The current count is: ${this.#props[countName]}`;
+          });
+        })(this.inputElement);
 
-					element.addEventListener("input", (event) => {
-						const value = parseInt(event.target.value, 10);
+      hasProps &&
+        this.#eventTarget.addEventListener("prop-changed", (event) => {
+          const countName = this.dataset.countName ?? "count";
 
-						if (!isNaN(value)) {
-							this.#props[countName] = value;
-						}
-					});
+          if (event.detail.key !== countName) return;
 
-					element.addEventListener("focus", (event) => {
-						event.target.value = `${this.#props[countName]}`;
-					});
+          const value = event.detail.value;
 
-					element.addEventListener("blur", (event) => {
-						event.target.value = `The current count is: ${this.#props[countName]}`;
-					});
-				}
-			})();
-		}
-	};
+          const h1Text = `The current count is: ${value}`;
+
+          ((element) => {
+            element.textContent = h1Text;
+          })(this.h1Element);
+
+          ((element) => {
+            element.textContent = `increment: ${value}`;
+          })(this.incrementButton);
+
+          ((element) => {
+            element.textContent = `decrement: ${value}`;
+          })(this.decrementButton);
+
+          ((element) => {
+            if (element.matches(":focus")) return;
+
+            element.value = h1Text;
+          })(this.inputElement);
+        });
+    }
+  };
 };
