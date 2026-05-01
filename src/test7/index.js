139a918849d34378f437240876e79b78f31dc535
Pavel Sommer <pavel.sommer@gmail.com>
2026-05-02 01:40:49 +0200

    sync!

--- src/test7/index.js
+++ src/test7/index.js
@@ -1,220 +1,238 @@
+const state = new Proxy(
+	{},
+	{
+		set: (target, key, value, receiver) => {
+			const oldValue = target[key];
+
+			if (oldValue === value) return true;
+			const result = Reflect.set(target, key, value, receiver);
+
+			return result;
+		},
+	},
+);
+
 class component {
-  static assign(target, source) {
-    const { style, dataset, ...props } = source;
+	static assign(target, source) {
+		const { style, dataset, ...props } = source;
 
-    style && Object.assign(target.style, style);
-    dataset && Object.assign(target.dataset, dataset);
+		style && Object.assign(target.style, style);
+		dataset && Object.assign(target.dataset, dataset);
 
-    Object.assign(target, props);
-  }
+		Object.assign(target, props);
+	}
 }
 
 const createTemplate = (innerHTML) => {
-  const template = document.createElement("template");
+	const template = document.createElement("template");
 
-  template.setHTMLUnsafe(innerHTML);
+	template.setHTMLUnsafe(innerHTML);
 
-  return template;
+	return template;
 };
 
 const useTemplate = (template, refs) => {
-  const fragment = template.content.cloneNode(true);
+	const fragment = template.content.cloneNode(true);
 
-  return {
-    fragment,
-    ...refs(fragment),
-  };
+	return {
+		fragment,
+		...refs(fragment),
+	};
 };
 
 const createState = (props = {}) => {
-  const eventTarget = new EventTarget();
-  const handler = {
-    set: (target, key, value, receiver) => {
-      const oldValue = target[key];
+	const eventTarget = new EventTarget();
+	const handler = {
+		set: (target, key, value, receiver) => {
+			const oldValue = target[key];
 
-      if (oldValue === value) return true;
-      const result = Reflect.set(target, key, value, receiver);
+			if (oldValue === value) return true;
+			const result = Reflect.set(target, key, value, receiver);
 
-      eventTarget.dispatchEvent(
-        new CustomEvent("prop-changed", {
-          detail: {
-            target,
-            key,
-            oldValue,
-            value,
-            receiver,
-          },
-        }),
-      );
+			eventTarget.dispatchEvent(
+				new CustomEvent("prop-changed", {
+					detail: {
+						target,
+						key,
+						oldValue,
+						value,
+						receiver,
+					},
+				}),
+			);
 
-      return result;
-    },
-  };
+			return result;
+		},
+	};
 
-  const proxy = new Proxy(props, handler);
+	const proxy = new Proxy(props, handler);
 
-  return {
-    props: proxy,
-    subscribe: (listener, abortSignal) =>
-      eventTarget.addEventListener("prop-changed", listener, {
-        signal: abortSignal,
-      }),
-  };
+	return {
+		props: proxy,
+		subscribe: (listener, abortSignal) =>
+			eventTarget.addEventListener("prop-changed", listener, {
+				signal: abortSignal,
+			}),
+	};
 };
 
 const createComponent = (template) => {
-  return class extends HTMLElement {
-    #abortController = new AbortController();
-    #abortSignal = null;
+	return class extends HTMLElement {
+		#abortController = new AbortController();
+		#abortSignal = null;
 
-    refs = {};
-    props = {};
+		refs = {};
+		props = {};
 
-    get abortSignal() {
-      return this.#abortSignal;
-    }
+		get abortSignal() {
+			return this.#abortSignal;
+		}
 
-    constructor() {
-      super();
-      this.attachShadow({ mode: "open" });
-    }
+		constructor() {
+			super();
+			this.attachShadow({ mode: "open" });
+		}
 
-    connectedCallback() {
-      this.#abortSignal = this.#abortController.signal;
+		connectedCallback() {
+			this.#abortSignal = this.#abortController.signal;
 
-      const { fragment, ...refs } = template();
-      this.refs = refs;
+			const { fragment, ...refs } = template();
+			this.refs = refs;
 
-      this.shadowRoot.appendChild(fragment);
-      this.connectedCompleteCallback();
-    }
+			this.shadowRoot.appendChild(fragment);
+			this.connectedCompleteCallback();
+		}
 
-    disconnectedCallback() {
-      this.#abortController.abort();
-      this.#abortController = new AbortController();
+		disconnectedCallback() {
+			this.#abortController.abort();
+			this.#abortController = new AbortController();
 
-      this.#abortSignal = this.#abortController.signal;
+			this.#abortSignal = this.#abortController.signal;
 
-      this.refs = {};
-      this.props = {};
-    }
+			this.refs = {};
+			this.props = {};
+		}
 
-    connectedCompleteCallback() {}
+		connectedCompleteCallback() {}
 
-    attributeChangedCallback(name, oldValue, newValue) {}
-  };
+		attributeChangedCallback(name, oldValue, newValue) {}
+	};
 };
 
 class MyTextBox extends HTMLElement {
-  static #template = (() => {
-    const template = createTemplate(`<div><input type="text" /></div>`);
+	static #template = (() => {
+		const template = createTemplate(`<div><input type="text" /></div>`);
 
-    return () =>
-      useTemplate(template, (fragment) => ({
-        div: fragment.children[0],
-        input: fragment.children[0].children[0],
-      }));
-  })();
+		return () =>
+			useTemplate(template, (fragment) => ({
+				div: fragment.children[0],
+				input: fragment.children[0].children[0],
+			}));
+	})();
 
-  connectedCallback() {
-    const { fragment, ...template } = MyTextBox.#template();
+	connectedCallback() {
+		const { fragment, ...template } = MyTextBox.#template();
 
-    template.input.value = this.dataset.value ?? "";
-    template.input.addEventListener("input", (event) => {});
+		template.input.value = this.dataset.value ?? "";
+		template.input.addEventListener("input", (event) => {
+			this.dataset.id &&
+				(state[`${this.dataset.id}.value`] = event.target.value);
+			console.log(state);
+		});
 
-    this.append(fragment);
-  }
+		this.append(fragment);
+	}
 }
 
 class MyDashboard extends HTMLElement {
-  static #template = (() => {
-    const template = createTemplate(`<h1>hello!</h1><my-textbox></my-textbox>`);
+	static #template = (() => {
+		const template = createTemplate(`<h1>hello!</h1><my-textbox></my-textbox>`);
 
-    return () =>
-      useTemplate(template, (fragment) => ({
-        h1: fragment.children[0],
-        textBox: fragment.children[1],
-      }));
-  })();
+		return () =>
+			useTemplate(template, (fragment) => ({
+				h1: fragment.children[0],
+				textBox: fragment.children[1],
+			}));
+	})();
 
-  connectedCallback() {
-    const { fragment, ...template } = MyDashboard.#template();
+	connectedCallback() {
+		const { fragment, ...template } = MyDashboard.#template();
 
-    template.h1.textContent = this.dataset.text ?? "";
-    template.textBox.dataset.value = this.dataset.value ?? "";
+		template.h1.textContent = this.dataset.text ?? "";
+		template.textBox.dataset.value = this.dataset.value ?? "";
 
-    this.append(fragment);
-  }
+		this.append(fragment);
+	}
 }
 
 class MyItem extends HTMLElement {
-  static #template = (() => {
-    const template = createTemplate(`<li></li>`);
+	static #template = (() => {
+		const template = createTemplate(`<li></li>`);
 
-    return () =>
-      useTemplate(template, (fragment) => ({
-        li: fragment.children[0],
-      }));
-  })();
+		return () =>
+			useTemplate(template, (fragment) => ({
+				li: fragment.children[0],
+			}));
+	})();
 
-  connectedCallback() {
-    const { fragment, ...template } = MyItem.#template();
+	connectedCallback() {
+		const { fragment, ...template } = MyItem.#template();
 
-    template.li.textContent = this.dataset.text;
+		template.li.textContent = this.dataset.text;
 
-    this.append(fragment);
-  }
+		this.append(fragment);
+	}
 }
 
 class MyList extends HTMLElement {
-  static #container = (() => {
-    const template = createTemplate(`<h1>List</h1><hr /><ul></ul>`);
+	static #container = (() => {
+		const template = createTemplate(`<h1>List</h1><hr /><ul></ul>`);
 
-    return () =>
-      useTemplate(template, (fragment) => ({
-        ul: fragment.children[2],
-      }));
-  })();
+		return () =>
+			useTemplate(template, (fragment) => ({
+				ul: fragment.children[2],
+			}));
+	})();
 
-  static #item = (() => {
-    const template = createTemplate(`<my-item></my-item>`);
+	static #item = (() => {
+		const template = createTemplate(`<my-item></my-item>`);
 
-    return (callback) =>
-      useTemplate(template, (fragment) => {
-        const myItem = fragment.children[0];
-        const setText = (text) => (myItem.dataset.text = text);
-        const setStyle = (style) => Object.assign(myItem.style, style);
+		return (callback) =>
+			useTemplate(template, (fragment) => {
+				const myItem = fragment.children[0];
+				const setText = (text) => (myItem.dataset.text = text);
+				const setStyle = (style) => Object.assign(myItem.style, style);
 
-        callback && callback({ myItem, setText, setStyle });
+				callback && callback({ myItem, setText, setStyle });
 
-        return {
-          myItem,
-        };
-      });
-  })();
+				return {
+					myItem,
+				};
+			});
+	})();
 
-  connectedCallback() {
-    const { fragment, ...container } = MyList.#container();
-    const itemFragment = document.createDocumentFragment();
+	connectedCallback() {
+		const { fragment, ...container } = MyList.#container();
+		const itemFragment = document.createDocumentFragment();
 
-    for (let i = 0; i < 400; i++) {
-      const { fragment, ...item } = MyList.#item(
-        ({ myItem, setText, setStyle }) => {
-          setText(`Item ${i}`);
-          setStyle({
-            fontSize: "1.3rem",
-            fontWeight: "700",
-            color: "blue",
-          });
-        },
-      );
+		for (let i = 0; i < 400; i++) {
+			const { fragment, ...item } = MyList.#item(
+				({ myItem, setText, setStyle }) => {
+					setText(`Item ${i}`);
+					setStyle({
+						fontSize: "1.3rem",
+						fontWeight: "700",
+						color: "blue",
+					});
+				},
+			);
 
-      itemFragment.append(fragment);
-    }
+			itemFragment.append(fragment);
+		}
 
-    container.ul.append(itemFragment);
-    this.append(fragment);
-  }
+		container.ul.append(itemFragment);
+		this.append(fragment);
+	}
 }
 
 customElements.define("my-textbox", MyTextBox);
