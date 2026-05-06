942422a2593ef6b29a1361754ab16b9c2687b627
paYa <pavel.sommer@gmail.com>
2026-05-07 00:51:06 +0200

    Update index.html and refactor component structure
    - Switch to test9 components with button-red and div-red
    - Fix indentation from tabs to spaces
    - Comment out test8 stylesheet and script tags
    - Add Object.assignDeep utility for deep property assignment
    - Refactor Button component to use defineComponent factory pattern
    - Add abort controller for cleanup on disconnectedCallback

--- src/test9/Components/Button/index.js
+++ src/test9/Components/Button/index.js
@@ -1,16 +1,48 @@
-export default class Self extends HTMLButtonElement {
-	constructor() {
-		super();
-		console.log("abcd");
-	}
+const defineComponent = (baseClass, tagName, extendName, body) => {
+  class Self extends baseClass {
+    #abortController = new AbortController();
+    #abortSignal = null;
 
-	connectedCallback() {
-		Object.assign(this.style, {
-			padding: "0.5rem 1rem",
-			cursor: "pointer",
-			color: "green",
-		});
-	}
-}
+    get abortSignal() {
+      return this.#abortSignal;
+    }
 
-customElements.define("my-button", Self, { extends: "button" });
+    connectedCallback() {
+      this.#abortSignal = this.#abortController.signal;
+      !this.hasAttribute("is") && this.setAttribute("is", tagName);
+
+      Object.assignDeep(this, {
+        textContent: "Hello!",
+      });
+    }
+
+    disconnectedCallback() {
+      this.#abortController.abort();
+      this.#abortController = new AbortController();
+      this.#abortSignal = this.#abortController.signal;
+    }
+  }
+
+  const impl = body(Self);
+
+  customElements.define(tagName, impl, { extends: extendName });
+
+  return impl;
+};
+
+export default defineComponent(
+  HTMLButtonElement,
+  "button-red",
+  "button",
+  (Base) => {
+    return class Self extends Base {
+      connectedCallback() {
+        super.connectedCallback();
+
+        this.addEventListener("click", (event) => console.log("Hello!"), {
+          signal: this.abortSignal,
+        });
+      }
+    };
+  },
+);
--- src/test9/index.js
+++ src/test9/index.js
@@ -1,1 +1,11 @@
+Object.assignDeep =
+  Object.assignDeep ||
+  function (target, source) {
+    const { style, dataset, ...props } = source || {};
+
+    props && target && Object.assign(target, props);
+    dataset && target?.dataset && Object.assign(target.dataset, dataset);
+    style && target?.style && Object.assign(target.style, style);
+  };
+
 await import("./Components/Button");
