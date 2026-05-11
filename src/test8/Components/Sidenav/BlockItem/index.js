f8809ae586483cc2e708d77df8b28470683419fa
paYa <pavel.sommer@gmail.com>
2026-05-12 00:49:34 +0200

    sync!

--- src/test8/Components/Sidenav/BlockItem/index.js
+++ src/test8/Components/Sidenav/BlockItem/index.js
@@ -3,20 +3,10 @@ import { createTemplate, useTemplate } from "./../../../Core";
 
 await import("./../Item/Title");
 
-const props = new Proxy(
-	{},
-	{
-		set: (target, key, value, receiver) => {
-			const result = Reflect.set(target, key, value, receiver);
+const titleTemplate = createTemplate(`<div is="sidenav-item-title">
+</div>`);
 
-			return result;
-		},
-	},
-);
-
-const contentTemplate = createTemplate(`<div is="sidenav-item-title">
-</div>
-<div class="items">
+const contentTemplate = createTemplate(`<div class="items">
 	<div class="items__inner">
 		<ul>
 		</ul>
@@ -24,56 +14,61 @@ const contentTemplate = createTemplate(`<div is="sidenav-item-title">
 </div>`);
 
 export default class Self extends HTMLLIElement {
-	static #createContent = (sidenav, options) => {
-		const { fragment, titleEl, itemsEl } = useTemplate(contentTemplate, (fragment) => {
-			return {
-				titleEl: fragment.children[0],
-				itemsEl: fragment.children[1],
-			};
-		});
+  static #renderTitle = (sidenav, options) => {
+    const { fragment, titleEl } = useTemplate(titleTemplate, (fragment) => {
+      return {
+        titleEl: fragment.children[0],
+      };
+    });
 
-		titleEl.textContent = options.textContent ?? "";
-		titleEl.addEventListener("click", () => {
-			console.log("Hello!");
-		});
+    titleEl.textContent = options.textContent ?? "";
+    titleEl.addEventListener("click", () => {
+      console.log("Hello!");
+    });
 
-		return fragment;
-	};
+    return fragment;
+  };
 
-	connectedCallback() {
-		this.#connectState();
-		this.#render();
-	}
+  static #renderContent = (sidenav, options) => {
+    const { fragment, itemsEl } = useTemplate(contentTemplate, (fragment) => {
+      return {
+        itemsEl: fragment.children[0],
+      };
+    });
 
-	async #connectState() {
-		const { props, watch } = (await import("./../../Sidebar/State")).default;
+    return fragment;
+  };
 
-		this.#update(props.blockId === this.dataset.id);
-		this.classList.add("mounted");
+  connectedCallback() {
+    this.#connectState();
+    this.#render();
+  }
 
-		watch("blockId", (event) => {
-			this.#update(event.detail.value === this.dataset.id);
-		});
-	}
+  async #connectState() {
+    const { props, watch } = (await import("./../../Sidebar/State")).default;
 
-	async #render() {
-		const sidenav = (await import("./../Items")).default;
-		const refs = sidenav.refs[this.dataset.id];
-		const items = sidenav.items;
+    this.#update(props.blockId === this.dataset.id);
+    this.classList.add("mounted");
 
-		for (const ref of refs) {
-			this.append(
-				Self.#createContent(sidenav, {
-					textContent: items[ref].title,
-				}),
-			);
-		}
-	}
+    watch("blockId", (event) => {
+      this.#update(event.detail.value === this.dataset.id);
+    });
+  }
 
-	#update(active) {
-		if (active) this.classList.add("active");
-		else this.classList.remove("active");
-	}
+  async #render() {
+    const sidenav = (await import("./../Items")).default;
+    const refs = sidenav.refs[this.dataset.id];
+    const items = sidenav.items;
+
+    for (const ref of refs) {
+      this.append(Self.#renderContent(sidenav, {}));
+    }
+  }
+
+  #update(active) {
+    if (active) this.classList.add("active");
+    else this.classList.remove("active");
+  }
 }
 
 customElements.define("sidenav-block-item", Self, { extends: "li" });
