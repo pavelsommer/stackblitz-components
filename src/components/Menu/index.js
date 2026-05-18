14757bf84a65f1a29e0176e0b9a0f9710ed782de
paYa <pavel.sommer@gmail.com>
2026-05-18 23:58:51 +0200

    sync!

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,38 +1,61 @@
-import { createTemplate, useTemplate, createBehavior, createFragment, useState } from "@lib";
+import {
+  createTemplate,
+  useTemplate,
+  createBehavior,
+  createFragment,
+  useState,
+} from "@lib";
 import { getItems } from "@services/Sidenav/Items";
 
-const template = createTemplate(`<li is="ui-menu-item" class="ui-menu-item"></li>`);
+const template = createTemplate(
+  `<li is="ui-menu-item" class="ui-menu-item"></li>`,
+);
 
 export default (Base) =>
-	class Self extends createBehavior(Base) {
-		get level() {
-			return parseInt(this.dataset.level ?? "1");
-		}
+  class Self extends createBehavior(Base) {
+    get level() {
+      return parseInt(this.dataset.level ?? "1");
+    }
 
-		async mounted() {
-			const { props, watch } = await useState({
-				id: this.dataset.stateId,
-			});
+    async mounted() {
+      const { props, watch } = await useState({
+        id: this.dataset.stateId,
+      });
 
-			const items = getItems(this.dataset.id);
+      const items = getItems(this.dataset.id);
 
-			this.append(
-				createFragment(items[0], (item, index) => {
-					const props = {
-						className: index === 0 ? "ui-menu-item expanded" : "ui-menu-item",
+      this.append(
+        createFragment(items[0], (item, index) => {
+          const props = {
+            className:
+              this.level === 1 && index === 0
+                ? "ui-menu-item expanded"
+                : "ui-menu-item",
 
-						dataset: {
-							id: item,
-							label: items[1][item].label,
-							level: this.level,
-						},
-					};
+            dataset: {
+              id: item,
+              label: items[1][item].label,
+              level: this.level,
+            },
+          };
 
-					return useTemplate(template, (fragment) => {
-						const element = fragment.children[0];
-						Object.assignProps(element, props);
-					}).fragment;
-				}),
-			);
-		}
-	};
+          return useTemplate(template, (fragment) => {
+            const menuItem = fragment.children[0];
+            Object.assignProps(menuItem, props);
+
+            // TODO: move this to Item component?
+            props.dataset.level === 1 &&
+              watch(
+                "sidenavArea",
+                (event) => {
+                  if (event.detail.value === item)
+                    menuItem.classList.add("expanded");
+                  else menuItem.classList.remove("expanded");
+                },
+                this.abortSignal,
+              );
+          }).fragment;
+        }),
+      );
+    }
+  };
--- src/components/State/index.js
+++ src/components/State/index.js
@@ -1,34 +1,34 @@
-import { createState } from "./../../lib";
+import { createState } from "@lib";
 
 export default class Self extends HTMLElement {
-	#props = null;
-	#watch = null;
+  #props = null;
+  #watch = null;
 
-	#options = {
-		self: {
-			style: {
-				display: "none",
-			},
-		},
-	};
+  #options = {
+    self: {
+      style: {
+        display: "none",
+      },
+    },
+  };
 
-	get state() {
-		return {
-			props: this.#props,
-			watch: this.#watch,
-		};
-	}
+  get state() {
+    return {
+      props: this.#props,
+      watch: this.#watch,
+    };
+  }
 
-	connectedCallback() {
-		const props = {};
+  connectedCallback() {
+    const props = {};
 
-		Object.assign(props, this.dataset);
-		Object.assignProps(this, this.#options.self);
+    Object.assign(props, this.dataset);
+    Object.assignProps(this, this.#options.self);
 
-		({ props: this.#props, watch: this.#watch } = createState(props));
-	}
+    ({ props: this.#props, watch: this.#watch } = createState(props));
+  }
 
-	setValue(propName, propValue) {
-		this.#props[propName] = propValue;
-	}
+  setValue(propName, propValue) {
+    this.#props[propName] = propValue;
+  }
 }
